e498049ff61e5b7dea13dd117efe3b0fd445edc1
hanumds <hanumdshafitri@gmail.com>
2024-06-29 10:33:01 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: '#425C59',
+        customGreen2: '#3D5653',
+      },
+    },
+  },
+  plugins: [],
+}
+
